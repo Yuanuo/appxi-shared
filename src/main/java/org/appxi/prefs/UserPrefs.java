750271824a1296b5b501cf5a24c51e7ab8b3c90d
Yuanuox <yuanuox@outlook.com>
2021-02-26 11:06:29 +0800

    移除appDir，目前似乎只在JavaFx程序中会出现此属性需求

--- src/main/java/org/appxi/prefs/UserPrefs.java
+++ src/main/java/org/appxi/prefs/UserPrefs.java
@@ -13,7 +13,6 @@ public abstract class UserPrefs {
     private static final Path _workDir = Path.of("").toAbsolutePath();
     private static Path _dataDir = Path.of(System.getProperty("user.home"));
     private static Path _confDir = _workDir.resolve(".config");
-    private static Path _appDir = null;
 
     public static void setupDataDirectory(Path dataDir, String confDirName) {
         _dataDir = null != dataDir ? dataDir : _dataDir;
@@ -40,38 +39,5 @@ public abstract class UserPrefs {
 
     public static Path cacheDir() {
         return _dataDir.resolve(".cached");
-    }
-
-    public static Path appDir() {
-        if (null != _appDir)
-            return _appDir;
-
-        synchronized (_workDir) {
-            if (null != _appDir)
-                return _appDir;
-
-            String appDir = System.getenv("app-dir");
-            if (null == appDir)
-                appDir = System.getProperty("jpackage.app-dir");
-
-            if (null == appDir) {
-                appDir = System.getProperty("jpackage.app-path");
-                if (null != appDir) {
-                    String osName = System.getProperty("os.name").toLowerCase();
-                    Path appPath = Path.of(appDir).getParent();
-                    if (osName.contains("win")) {
-                        appDir = appPath.resolve("app").toString();
-                    } else if (osName.contains("mac")) {
-
-                    } else {
-                        appDir = appPath.resolve("lib").toString();
-                    }
-                }
-            }
-            if (null == appDir)
-                appDir = "";
-            _appDir = Path.of(appDir).toAbsolutePath();
-        }
-        return _appDir;
     }
 }

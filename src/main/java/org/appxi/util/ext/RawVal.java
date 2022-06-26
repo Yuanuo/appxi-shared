a180a667cf1e4e6d94e53fc03435e656a10ea4ba
Yuanuox <yuanuox@outlook.com>
2022-06-26 21:11:52 +0800

    增加便捷方法

--- src/main/java/org/appxi/util/ext/RawVal.java
+++ src/main/java/org/appxi/util/ext/RawVal.java
@@ -19,4 +19,19 @@ public record RawVal<T>(T value, String title) {
     public String toString() {
         return title;
     }
+
+    /**
+     * alias to title
+     */
+    public String key() {
+        return title;
+    }
+
+    public static <V> RawVal<V> kv(String key, V value) {
+        return new RawVal<>(value, key);
+    }
+
+    public static <V> RawVal<V> vk(V value, String key) {
+        return new RawVal<>(value, key);
+    }
 }
